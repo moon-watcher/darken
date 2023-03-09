e242b0384a9a84120d5fb8407eae588a8389e4c6
langares <langares@yahoo.es>
2023-03-09 18:17:00 +0100

    Apply FEA

--- darken.c
+++ darken.c
@@ -2,7 +2,14 @@
 
 static int exitCode = 0;
 
-int darken(const deState_t *initialState, void (*constructor)(deEntity_t *const))
+const DarkenAPI    *const darken    = & ( DarkenAPI    ) { darken_init, darken_end };
+const deSystemAPI  *const deSystem  = & ( deSystemAPI  ) { deSystem_init,  deSystem_update,    deSystem_end    };
+const deManagerAPI *const deManager = & ( deManagerAPI ) { deManager_init, deManager_update,   deManager_end   };
+const deEntityAPI  *const deEntity  = & ( deEntityAPI  ) { deEntity_new,   deEntity_set_state, deEntity_delete };
+
+
+
+int darken_init(const deState_t *initialState, void (*constructor)(deEntity_t *const))
 {
     exitCode = 0;
     int *x = &exitCode;
