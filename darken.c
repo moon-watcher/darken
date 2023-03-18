1399d2a872faaea46fb09bfb58aa4ea289846634
langares <langares@yahoo.es>
2023-03-18 05:07:26 +0100

    Update darken.c

--- darken.c
+++ darken.c
@@ -4,19 +4,13 @@ static int _exit = 0;
 
 int darken(deDefinition_t *ed)
 {
-    const deDefinition_t manager_def = { .maxEntities = 1 };
-    
     _exit = 0;
     int *exitCode = &_exit;
     
-    deManager_t manager;
-    ed->manager = &manager;
-
-    deManager_init(ed->manager, &manager_def);
     deEntity_t *const e = deEntity_new(ed);
 
     while (!*exitCode)
-        e->state->update(e);
+        e->update(e);
 
     return *exitCode;
 }
