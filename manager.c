a11b219f2fcc5545ae36db675f3ef41d254dfb94
Moon-Watcher <langares@yahoo.es>
2023-04-21 00:51:30 +0200

    Add deManager_reset()

--- manager.c
+++ manager.c
@@ -29,6 +29,12 @@ void deManager_end(deManager_t *const m)
     free(m->entityList);
 }
 
+void deManager_reset(deManager_t *const m)
+{
+    while(m->free_pos)
+        deEntity_delete(m->entityList[0]);
+}
+
 void deManager_update(deManager_t *const m)
 {
     if (m->pause)
--- manager.h
+++ manager.h
@@ -20,6 +20,7 @@ typedef struct deManager_t
 
 void deManager_init       ( deManager_t *const, unsigned, unsigned );
 void deManager_end        ( deManager_t *const );
+void deManager_reset      ( deManager_t *const );
 void deManager_update     ( deManager_t *const );
 void deManager_pauseTimer ( deManager_t *const, unsigned );
 void deManager_pause      ( deManager_t *const );
