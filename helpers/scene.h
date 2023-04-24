ac79afa0924a0f2352f113927f0afab5a6dd0343
langares <langares@yahoo.es>
2023-04-24 03:20:18 +0200

    Update scene.h

--- helpers/scene.h
+++ helpers/scene.h
@@ -7,11 +7,11 @@
                                                                \
     const Scene NAME = { & NAME##_e, & NAME##_u, & NAME##_l };
     
-#define dehState_define_enter(NAME, ENTER)               \
+#define dehScene_define_enter(NAME, ENTER)               \
     static void NAME##_e (deEntity_t *const this) ENTER  \
     const deState_t NAME = { .enter = & NAME##_e };
     
-#define dehState_define_update(NAME, UPDATE)             \
+#define dehScene_define_update(NAME, UPDATE)             \
     static void NAME##_u (deEntity_t *const this) UPDATE \
     const deState_t NAME = { .update = & NAME##_u };
     
