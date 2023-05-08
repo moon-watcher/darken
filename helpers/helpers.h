f0cfc240f4ce6d1d89fd1eb6aa2de2906b8cbd9e
Moon-Watcher <langares@yahoo.es>
2023-05-08 22:59:28 +0200

    Update helpers.h

--- helpers/helpers.h
+++ helpers/helpers.h
@@ -3,7 +3,7 @@
 #define dehData(T) \
     T *const data = (T *const) &this->data
 
-#define REF(T, N, V) \
+#define dehDataRef(T, N, V) \
     T *const V = (T *const) &data->N
     
 #define dehDataComp(T, V) \
