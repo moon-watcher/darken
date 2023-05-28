c6abaa9bd8d0a5c74d2f9ed7da5961aa60cf59bc
Moon-Watcher <langares@yahoo.es>
2023-05-29 00:50:26 +0200

    dehEntity_delete

--- helpers/entity.h
+++ helpers/entity.h
@@ -1,4 +1,5 @@
 #pragma once
 
-#define dehEntity_change(S)  deEntity_change(this, S);
-#define dehEntity_set(S)     deEntity_set(this, S);
+#define dehEntity_change(S)  deEntity_change(this, S)
+#define dehEntity_set(S)     deEntity_set(this, S)
+#define dehEntity_delete()   deEntity_delete(this)
