2a220a46d24385885b89cb61a651cb9e4852dc67
MoonWatcherMD <langares@yahoo.es>
2023-06-05 22:39:28 +0200

    Update deSystem to apply upl_*

--- core/system.c
+++ core/system.c
@@ -1,49 +1,85 @@
 #include "../darken.h"
 
-void deSystem_init(deSystem_t *const s, deSystem_f const updateFn, unsigned maxItems, unsigned params)
+void deSystem_init(deSystem_t *const this, deSystem_f const updateFn, unsigned params, unsigned maxItems)
 {
-    s->updateFn = updateFn;
-    s->maxItems = maxItems;
-    s->params = params;
-    s->list = malloc(sizeof(void *) * maxItems * params);
-    s->length = 0;
+    this->updateFn = updateFn;
+    this->maxItems = maxItems;
+    this->params = params;
+
+    upl_init(&this->upl, this->maxItems * this->params);
 }
 
-void deSystem_update(deSystem_t *const s)
+void deSystem_add(deSystem_t *const this, ...)
 {
-    if (s == NULL || s->length == 0)
-        return;
+    va_list ap;
+    va_start(ap, this);
 
-    if (s->length >= s->maxItems * s->params)
-        return;
+    for (unsigned i = 0; i < this->params; i++)
+        upl_add(&this->upl, va_arg(ap, void *const));
 
-    s->updateFn(s);
-    s->length = 0;
+    va_end(ap);
 }
 
-void deSystem_end(deSystem_t *const s)
+void deSystem_delete(deSystem_t *const this, void *const data)
 {
-    free(s->list);
+    upl_bulk_removeByData(&this->upl, data, this->params);
 }
 
-void deSystem_add(deSystem_t *const s, void *const array[])
+void deSystem_update(deSystem_t *const this)
 {
-    unsigned const max = s->params;
-    void **const list = s->list;
+    upl_bulk_foreach(&this->upl, this->updateFn, this->params);
+}
+
+void deSystem_end(deSystem_t *const this)
+{
+    upl_end(&this->upl);
+}
+
+
+// void deSystem_end(deSystem_t *const s)
+// {
+//     free(s->list);
+// }
+
+// void deSystem_update(deSystem_t *const s)
+// {
+//     if (s == NULL || s->length == 0)
+//         return;
+
+//     if (s->length >= s->maxItems * s->params)
+//         return;
+
+//     s->updateFn(s);
+//     s->length = 0;
+// }
+
+// void deSystem_init(deSystem_t *const s, deSystem_f const updateFn, unsigned maxItems, unsigned params)
+// {
+//     s->updateFn = updateFn;
+//     s->maxItems = maxItems;
+//     s->params = params;
+//     s->list = malloc(sizeof(void *) * maxItems * params);
+//     s->length = 0;
+// }
+
+// void deSystem_info(deSystem_t *const s)
+// {
+//     VDP_resetScreen();
+
+//     drawText("=SYSTEM=", 0, 0);
+//     drawText("MAX:    ", 0, 1);
+//     drawUInt(s->maxItems, 9, 1, 4);
+//     drawText("LENGTH: ", 0, 2);
+//     drawUInt(s->length, 9, 2, 4);
+
+//     waitMs(20000);
+// }
+
+// void deSystem_add(deSystem_t *const s, void *const array[])
+// {
+//     unsigned const max = s->params;
+//     void **const list = s->list;
     
-    for (unsigned i = 0; i < max; i++)
-        list[s->length++] = array[i];
-}
-
-void deSystem_info(deSystem_t *const s)
-{
-    VDP_resetScreen();
-
-    drawText("=SYSTEM=", 0, 0);
-    drawText("MAX:    ", 0, 1);
-    drawUInt(s->maxItems, 9, 1, 4);
-    drawText("LENGTH: ", 0, 2);
-    drawUInt(s->length, 9, 2, 4);
-
-    waitMs(20000);
-}
+//     for (unsigned i = 0; i < max; i++)
+//         list[s->length++] = array[i];
+// }
--- core/system.h
+++ core/system.h
@@ -1,32 +1,50 @@
 #pragma once
 
+// typedef struct deSystem_t deSystem_t;
+// typedef void (*deSystem_f) (deSystem_t *const);
+
+// typedef struct deSystem_t
+// {
+//     void **list;
+//     unsigned length;
+//     deSystem_f updateFn;
+//     unsigned maxItems;
+//     unsigned params;
+// } deSystem_t;
+
+// void deSystem_init   ( deSystem_t *const, deSystem_f const, unsigned, unsigned);
+// void deSystem_update ( deSystem_t *const );
+// void deSystem_end    ( deSystem_t *const );
+// void deSystem_info   ( deSystem_t *const );
+// void deSystem_add    ( deSystem_t *const, void *const[] );
+
+// define deSystem_function(FUNC, CODE) 
+//     void FUNC(deSystem_t *const s)    
+//     {                                 
+//         int i = 0;                    
+//         int len = s->length;          
+//         while (i < len)               
+//             CODE                      
+//     }
+
+// # define deSystem_get(T, V) 
+//     T *const V = (T *const)s->list[i++]
+
+#include "../libs/upl.h"
+
 typedef struct deSystem_t deSystem_t;
 typedef void (*deSystem_f) (deSystem_t *const);
 
 typedef struct deSystem_t
 {
-    void **list;
-    unsigned length;
     deSystem_f updateFn;
     unsigned maxItems;
     unsigned params;
+    upl_t upl;
 } deSystem_t;
 
-void deSystem_init   ( deSystem_t *const, deSystem_f const, unsigned, unsigned);
-void deSystem_update ( deSystem_t *const );
-void deSystem_end    ( deSystem_t *const );
-void deSystem_info   ( deSystem_t *const );
-void deSystem_add    ( deSystem_t *const, void *const[] );
-
-#define deSystem_function(FUNC, CODE) \
-    void FUNC(deSystem_t *const s)    \
-    {                                 \
-        int i = 0;                    \
-        int len = s->length;          \
-        while (i < len)               \
-            CODE                      \
-    }
-
-#define deSystem_get(T, V) \
-    T *const V = (T *const)s->list[i++]
-    
+void deSystem_init(deSystem_t *const this, deSystem_f const updateFn, unsigned params, unsigned maxItems);
+void deSystem_add(deSystem_t *const this, ...);
+void deSystem_delete(deSystem_t *const this, void *const data);
+void deSystem_update(deSystem_t *const this);
+void deSystem_end(deSystem_t *const this);
