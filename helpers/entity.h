608034b19d114e636c3edb535308db45abb187d9
MoonWatcherMD <langares@yahoo.es>
2023-06-12 22:36:19 +0200

    Helper

--- helpers/entity.h
+++ helpers/entity.h
@@ -3,3 +3,4 @@
 #define dehEntity_change(S)  deEntity_change(this, S)
 #define dehEntity_set(S)     deEntity_set(this, S)
 #define dehEntity_delete()   deEntity_delete(this)
+#define dehEntity_update()   deEntity_update(this)
