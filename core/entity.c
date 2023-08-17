16c14767ef9d4da7251a0445eb401c06ad9f91fe
MoonWatcherMD <langares@yahoo.es>
2023-08-17 11:02:31 +0200

    Remove unneeded headers

--- core/entity.c
+++ core/entity.c
@@ -2,9 +2,6 @@
 #include "entity.h"
 #include "manager.h"
 
-#include "../config/free.h"
-#include "../config/malloc.h"
-
 de_entity *de_entity_new(const de_state *const s)
 {
     return de_manager_entity_create(0, s);
