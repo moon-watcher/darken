cfdcd6d8610fbacf64518be418f81f33e6f124b2
MoonWatcherMD <langares@yahoo.es>
2023-09-23 02:02:47 +0200

    Add DARKEN_MANAGER_DATA on manager.h

--- core/manager.h
+++ core/manager.h
@@ -4,17 +4,23 @@ typedef struct de_entity de_entity;
 typedef struct de_state de_state;
 
 #include "../libs/culist.h"
+#include "../config/darken.h"
 
 typedef struct de_manager
 {
     culist cul;
     unsigned char pause : 1;
+#if DARKEN_MANAGER_DATA
+    void *data;
+#endif
 } de_manager;
 
 void de_manager_init(de_manager *const, unsigned int, unsigned int);
 void de_manager_end(de_manager *const);
 void de_manager_reset(de_manager *const);
 void de_manager_update(de_manager *const);
+void *de_manager_data(de_manager *const, unsigned int);
+void de_manager_free(de_manager *const);
 
 de_entity *de_manager_entity_create(de_manager *const, const de_state *const);
 void de_manager_entity_delete(de_manager *const, de_entity *const);
