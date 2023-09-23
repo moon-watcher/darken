da7575420e350815a208adb0226fb91f3feecec3
MoonWatcherMD <langares@yahoo.es>
2023-09-23 02:03:13 +0200

    manager DARKEN_MANAGER_DATA and refactor

--- core/manager.c
+++ core/manager.c
@@ -7,60 +7,84 @@
 
 #include "../libs/culist.h"
 
-void de_manager_init(de_manager *const m, unsigned int maxEntities, unsigned int objectSize)
+void de_manager_init(de_manager *const this, unsigned int maxEntities, unsigned int objectSize)
 {
-    m->pause = 0;
-    culist_init(&m->cul, maxEntities, objectSize + sizeof(de_entity));
+    this->pause = 0;
+    culist_init(&this->cul, maxEntities, objectSize + sizeof(de_entity));
+    de_manager_free(this);
 }
 
-void de_manager_end(de_manager *const m)
+void de_manager_end(de_manager *const this)
 {
-    culist_end(&m->cul, de_state_destruct);
+    culist_end(&this->cul, de_state_destruct);
+    de_manager_free(this);
 }
 
-void de_manager_reset(de_manager *const m)
+void de_manager_reset(de_manager *const this)
 {
-    culist_reset(&m->cul, de_state_destruct);
+    culist_reset(&this->cul, de_state_destruct);
 }
 
-void de_manager_update(de_manager *const m)
+void de_manager_update(de_manager *const this)
 {
-    if (m->pause == 0)
-        culist_iterator(&m->cul, de_state_update);
+    if (this->pause == 0)
+        culist_iterator(&this->cul, de_state_update);
 }
 
-de_entity *de_manager_entity_create(de_manager *const m, const de_state *const s)
+void *de_manager_data(de_manager *const this, unsigned int size)
 {
-    de_entity *e;
+#if DARKEN_MANAGER_DATA
+    if (this->data == 0)
+        this->data = malloc(size);
 
-    if (m == 0)
-        e = malloc(sizeof(de_entity));
+    return this->data;
+#endif
+    return 0;
+}
+
+void de_manager_free(de_manager *const this)
+{
+#if DARKEN_MANAGER_DATA
+    if (this->data != 0)
+        return;
+
+    free(this->data);
+    this->data = 0;
+#endif
+}
+
+de_entity *de_manager_entity_create(de_manager *const this, const de_state *const state)
+{
+    de_entity *entity;
+
+    if (this == 0)
+        entity = malloc(sizeof(de_entity));
     else
     {
-        e = culist_add(&m->cul, 0);
-        memset(e->data, 0, m->cul.objectSize - sizeof(de_entity));
+        entity = culist_add(&this->cul, 0);
+        memset(entity->data, 0, this->cul.objectSize - sizeof(de_entity));
     }
 
-    e->xtor = (de_state *)s;
-    e->manager = m;
+    entity->xtor = (de_state *)state;
+    entity->manager = this;
 
-    de_state_set(e, s);
+    de_state_set(entity, state);
 
-    return e;
+    return entity;
 }
 
-void de_manager_entity_delete(de_manager *const m, de_entity *const e)
+void de_manager_entity_delete(de_manager *const this, de_entity *const entity)
 {
-    if (m != 0)
-        culist_remove(&m->cul, e, de_state_destruct);
+    if (this != 0)
+        culist_remove(&this->cul, entity, de_state_destruct);
     else
     {
-        de_state_destruct(e);
-        free(e);
+        de_state_destruct(entity);
+        free(entity);
     }
 }
 
-void de_manager_entity_iterator(de_manager *const m, void (*iterator)(de_entity *const e))
+void de_manager_entity_iterator(de_manager *const this, void (*iterator)(de_entity *const entity))
 {
-    culist_iterator(&m->cul, iterator);
+    culist_iterator(&this->cul, iterator);
 }
