c8de458621547fabceed862c219da8efde8c885a
MoonWatcherMD <langares@yahoo.es>
2023-09-24 03:13:37 +0200

    Refactor

--- implement/realloc.c
+++ implement/realloc.c
@@ -3,17 +3,20 @@
 
 void *de_implement_realloc(void *ptr, unsigned int size)
 {
+    if (ptr == 0)
+        return 0;
+
     void *p = malloc(size);
 
-    if (p != 0 && ptr != 0)
-    {
-        // unsigned int originalSize = ¿¿¿getsize(ptr)???;
-        // if (originalSize < size)
-        //     size = originalSize;
+    if (p == 0)
+        return 0;
+    
+    // unsigned int originalSize = getsize(ptr);
+    // if (originalSize < size)
+    //     size = originalSize;
 
-        memcpy(p, ptr, size);
-        free(ptr);
-    }
-
+    memcpy(p, ptr, size);
+    free(ptr);
+    
     return p;
 }
