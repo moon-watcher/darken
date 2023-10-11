c9cb25aea7b4921557887fed1a054d85e04ae4ed
MoonWatcherMD <langares@yahoo.es>
2023-10-11 09:27:46 +0200

    Add entity helpers

--- core/entity.h
+++ core/entity.h
@@ -22,5 +22,16 @@ typedef struct de_entity
 #endif
 } de_entity;
 
+#if DARKEN_ENTITY_DATA
+#define deh_entity_data(ENTITY, TYPE, DATA) \
+    TYPE *const DATA = (TYPE *const)&ENTITY->data
+
+#define deh_entity_var(DATA, TYPE, VAR1) \
+    TYPE *const VAR1 = (TYPE *const)&DATA->VAR1
+
+#define deh_entity_varas(DATA, TYPE, VAR1, VAR2) \
+    TYPE *const VAR2 = (TYPE *const)&DATA->VAR1
+#endif
+
 de_entity *de_entity_new(const de_state *const);
 void de_entity_delete(de_entity *const);
