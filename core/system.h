041f175b26e130632878d6b7c6085f35aed3ec57
langares@yahoo.es <langares@yahoo.es>
2024-04-14 21:42:48 +0200

    no message

--- core/system.h
+++ core/system.h
@@ -15,7 +15,6 @@ void de_system_init(de_system *const, de_system_f, unsigned);
 void *de_system_add(de_system *const, void *const);
 unsigned de_system_delete(de_system *const, void *const);
 void de_system_addEx(de_system *const, ...);
-void de_system_deleteEx(de_system *const, void *const); 
+void de_system_deleteEx(de_system *const, void *const);
 void de_system_update(de_system *const);
 void de_system_end(de_system *const);
-
