e8458b8bbeb644d2a22df14a9329f55cbda20cb9
langares@yahoo.es <langares@yahoo.es>
2024-04-28 23:03:30 +0200

    States must not be null

--- core/state.c
+++ core/state.c
@@ -7,20 +7,17 @@ __attribute__((always_inline)) inline void de_state_enter(de_state *const this, 
     if (this->dm_size != 0)
         entity->statedata = malloc(this->dm_size);
 
-    if (this->enter != 0)
-        this->enter(entity, entity->data, entity->statedata);
+    this->enter(entity, entity->data, entity->statedata);
 }
 
 __attribute__((always_inline)) inline void de_state_update(de_state *const this, de_entity *const entity)
 {
-    if (this->update != 0)
-        this->update(entity, entity->data, entity->statedata);
+    this->update(entity, entity->data, entity->statedata);
 }
 
 __attribute__((always_inline)) inline void de_state_leave(de_state *const this, de_entity *const entity)
 {
-    if (this->leave != 0)
-        this->leave(entity, entity->data, entity->statedata);
+    this->leave(entity, entity->data, entity->statedata);
 
     if (this->dm_size != 0)
         free(entity->statedata);
