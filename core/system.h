a308ab853b1377bfd35be72d523672c72a49d693
langares@yahoo.es <langares@yahoo.es>
2024-04-28 22:55:25 +0200

    Remove de_system_f

--- core/system.h
+++ core/system.h
@@ -2,16 +2,14 @@
 
 #include "../libs/uplist.h"
 
-typedef void (*de_system_f)();
-
-typedef struct de_system
+typedef struct
 {
-    de_system_f update;
+    void (*update)();
     unsigned params;
     uplist list;
 } de_system;
 
-void de_system_init(de_system *const, de_system_f, unsigned);
+void de_system_init(de_system *const, void (*)(), unsigned);
 void *de_system_add(de_system *const, void *const);
 unsigned de_system_delete(de_system *const, void *const);
 void de_system_addEx(de_system *const, ...);
