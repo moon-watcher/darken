343b12ee5e6e59c8592f3be9d54b6760e03f698f
langares@yahoo.es <langares@yahoo.es>
2024-05-07 22:24:23 +0200

    Safe state execs

--- core/state.c
+++ core/state.c
@@ -4,15 +4,18 @@
 
 __attribute__((always_inline)) inline void de_state_enter(de_state *const this, de_entity *const entity)
 {
-    this->enter(entity, entity->data);
+    if (this->enter != 0)
+        this->enter(entity, entity->data);
 }
 
 __attribute__((always_inline)) inline void de_state_update(de_state *const this, de_entity *const entity)
 {
-    this->update(entity, entity->data);
+    if (this->update != 0)
+        this->update(entity, entity->data);
 }
 
 __attribute__((always_inline)) inline void de_state_leave(de_state *const this, de_entity *const entity)
 {
-    this->leave(entity, entity->data);
+    if (this->leave != 0)
+        this->leave(entity, entity->data);
 }
