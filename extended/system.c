7fe522753da6d6deabae382318ae07320cac1534
langares@yahoo.es <langares@yahoo.es>
2024-05-25 17:55:26 +0200

    Extended functions

---
+++ extended/system.c
@@ -0,0 +1,22 @@
+#include "system.h"
+#include "../services/va_arg.h"
+
+void de_system_add_Ex(de_system *const this, ...)
+{
+    va_list ap;
+    va_start(ap, this);
+
+    for (int i = 0; i < this->params; i++)
+        uplist_add(&this->list, va_arg(ap, void *const));
+}
+
+void de_system_delete_Ex(de_system *const this, void *const data)
+{
+    int index = uplist_find(&this->list, data);
+
+    if (index < 0)
+        return;
+
+    this->list.count -= this->params;
+    memcpy(this->list.items[index], this->list.items[this->list.count], sizeof(void *) * this->params);
+}
---
+++ extended/system.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "../core/system.h"
+
+void de_system_add_Ex(de_system *const, ...);
+void de_system_delete_Ex(de_system *const, void *const);
