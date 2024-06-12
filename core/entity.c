1b91bdb6060ddc736c6e5a57c0050f87de41197c
langares@yahoo.es <langares@yahoo.es>
2024-06-13 01:31:38 +0200

    Use _nullf()

--- core/entity.c
+++ core/entity.c
@@ -20,14 +20,13 @@ de_entity *de_entity_set(de_entity *const this, de_state *state)
 
 void de_entity_setPolicy(de_entity *const this, unsigned type)
 {
-    void nullf() {}
     void *f0(de_entity *const e) { return e->state->update; }
     void *f1(de_entity *const e) { return e->state->update ?: e->xtor->update; }
     void *f2(de_entity *const e) { return e->xtor->update ?: e->state->update; }
     void *f3(de_entity *const e) { return e->xtor->update; }
     void *(*const funcs[])() = {f0, f1, f2, f3};
 
-    this->update = funcs[type](this) ?: nullf;
+    this->update = funcs[type](this) ?: _nullf;
 }
 
 void de_entity_updateState(de_entity *const this)
