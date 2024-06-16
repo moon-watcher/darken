f97061e807d8dcc3bb6d99b01b2e50e895e506ca
langares@yahoo.es <langares@yahoo.es>
2024-06-16 04:02:36 +0200

    WIP

--- stuff/vanilla/vanillamanager.c
+++ stuff/vanilla/vanillamanager.c
@@ -1,23 +1,23 @@
-#include "vanman.h"
+#include "vanillamanager.h"
 
-void vanman_init(vanman *const this, unsigned bytes, void(*update), void(*destroy))
+void vanillamanager_init(vanillamanager *const this, unsigned bytes, void(*update), void(*destroy))
 {
     uplist_initAlloc(&this->list, bytes);
     this->update = update;
     this->destroy = destroy;
 }
 
-void *vanman_new(vanman *const this)
+void *vanillamanager_new(vanillamanager *const this)
 {
     return uplist_alloc(&this->list);
 }
 
-void vanman_update(vanman *const this)
+void vanillamanager_update(vanillamanager *const this)
 {
     uplist_iterator(&this->list, this->update, 1);
 }
 
-unsigned vanman_delete(vanman *const this, void *const item)
+unsigned vanillamanager_delete(vanillamanager *const this, void *const item)
 {
     int index = uplist_find(&this->list, item);
 
@@ -29,14 +29,14 @@ unsigned vanman_delete(vanman *const this, void *const item)
     return uplist_remove(&this->list, item);
 }
 
-void vanman_reset(vanman *const this)
+void vanillamanager_reset(vanillamanager *const this)
 {
     uplist_iterator(&this->list, this->destroy, 1);
     uplist_reset(&this->list);
 }
 
-void vanman_end(vanman *const this)
+void vanillamanager_end(vanillamanager *const this)
 {
-    vanman_reset(this);
+    vanillamanager_reset(this);
     uplist_end(&this->list);
 }
