4ed46fb592cb5a81c8b7f7a6649ef49726554ea7
langares@yahoo.es <langares@yahoo.es>
2024-07-10 17:51:08 +0200

    de_entity changes

--- core/entity.c
+++ core/entity.c
@@ -1,24 +1,18 @@
 #include "entity.h"
 #include "../NOAPI/state.h"
 
-de_entity *de_entity_set(de_entity *const this, de_state *const state)
+void de_entity_set(de_entity *const this, de_state *const state)
 {
     this->state = state ?: &de_NOAPI_state_empty;
     this->ctrl = 3;
-
-    return this;
 }
 
-de_entity *de_entity_delay(de_entity *const this)
+void de_entity_delay(de_entity *const this)
 {
     this->ctrl = 1;
-
-    return this;
 }
 
-de_entity *de_entity_delete(de_entity *const this)
+void de_entity_delete(de_entity *const this)
 {
     this->ctrl = 2;
-
-    return this;
 }
