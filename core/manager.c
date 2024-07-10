e0834d997c532cef6016f40cb213762d29361eef
langares@yahoo.es <langares@yahoo.es>
2024-07-10 20:34:06 +0200

    manager

--- core/manager.c
+++ core/manager.c
@@ -11,30 +11,45 @@ de_entity *de_manager_new(de_manager *const this, void (*desctructor)())
 {
     de_entity *entity = uplist_alloc(&this->list);
 
-    if (entity == 0)
-        return 0;
-
-    entity->update = de_NOAPI_state_nullf;
-    entity->leave = de_NOAPI_state_nullf;
-    entity->destructor = desctructor ?: de_NOAPI_state_nullf;
-    entity->manager = this;
+    if (entity != 0)
+    {
+        entity->update = de_NOAPI_state_nullf;
+        entity->leave = de_NOAPI_state_nullf;
+        entity->destructor = desctructor ?: de_NOAPI_state_nullf;
+        entity->manager = this;
+    }
 
     return entity;
 }
 
 void de_manager_update(de_manager *const this)
 {
-    uplist_iterator(&this->list, de_NOAPI_entity_update, 1);
+    // uplist_iterator(&this->list, de_NOAPI_entity_update, 1);
+    uplist *const list = &this->list;
+    unsigned const count = list->count;
+
+    for (unsigned i = 0; i < count; i++)
+        de_NOAPI_entity_update(list->items[i]);
+    //
 }
 
 void de_manager_reset(de_manager *const this)
 {
-    uplist_iterator(&this->list, de_NOAPI_entity_destroy, 1);
-    uplist_reset(&this->list);
+    // uplist_iterator(&this->list, de_NOAPI_entity_destroy, 1);
+    uplist *const list = &this->list;
+    unsigned const count = list->count;
+
+    for (unsigned i = 0; i < count; i++)
+        de_NOAPI_entity_destroy(list->items[i]);
+    //
+
+    uplist_reset(list);
 }
 
 void de_manager_end(de_manager *const this)
 {
-    uplist_iterator(&this->list, de_NOAPI_entity_destroy, 1);
+    // uplist_iterator(&this->list, de_NOAPI_entity_destroy, 1);
+    de_manager_reset(this);
+
     uplist_end(&this->list);
 }
