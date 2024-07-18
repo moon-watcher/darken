a24d27c85ce2e3e3d8089e1a9cdbc074d38cafc1
langares@yahoo.es <langares@yahoo.es>
2024-07-18 02:07:41 +0200

    NOAPI/entity

--- NOAPI/entity.c
+++ NOAPI/entity.c
@@ -9,7 +9,7 @@ static void _set(de_entity *const);
 
 //
 
-void (*const de_NOAPI_entity_array[])(de_entity *const) = {_update, _delay, _delete, _set};
+const void (*const de_NOAPI_entity_array[])(de_entity *const) = {_update, _delay, _delete, _set};
 
 void de_NOAPI_entity_destroy(de_entity *const this)
 {
--- NOAPI/entity.h
+++ NOAPI/entity.h
@@ -2,6 +2,6 @@
 
 #include "../core/entity.h"
 
-void (*const de_NOAPI_entity_array[])(de_entity *const);
+extern const void (*const de_NOAPI_entity_array[])(de_entity *const);
 
 void de_NOAPI_entity_destroy(de_entity *const);
