5896f83b28886556e58401391488c02ee64c5562
langares@yahoo.es <langares@yahoo.es>
2024-09-08 13:07:59 +0200

    Manager refactor

--- core/manager.c
+++ core/manager.c
@@ -5,19 +5,18 @@
 #include "../priv/common.h"
 #include "../config.h"
 
-#define _DARKEN_MANAGER_EXEC(METHOD, ENTITY, DATA) \
-    if (METHOD != 0)                               \
-    {                                              \
-        METHOD(ENTITY, DATA);                      \
+#define _EXEC(METHOD, ENTITY, DATA) \
+    if (METHOD != 0)                \
+    {                               \
+        METHOD(ENTITY, DATA);       \
     }
 
 static void _nullf() {}
-static const de_state _empty = {_nullf, _nullf, _nullf};
 
 static void _destroy(de_entity *const this)
 {
-    _DARKEN_MANAGER_EXEC(this->leave, this, this->data);
-    _DARKEN_MANAGER_EXEC(this->destructor, this, this->data);
+    _EXEC(this->leave, this, this->data);
+    _EXEC(this->destructor, this, this->data);
 }
 
 //
@@ -50,11 +49,11 @@ static void _entity_delete(de_entity *const this, void *const data)
 
 static void _entity_set(de_entity *const this, void *const data)
 {
-    _DARKEN_MANAGER_EXEC(this->leave, this, data);
+    _EXEC(this->leave, this, data);
 
-    this->state = this->state ?: &_empty;
+    this->state = this->state ?: &(de_state){_nullf, _nullf, _nullf};
 
-    _DARKEN_MANAGER_EXEC(this->state->enter, this, data);
+    _EXEC(this->state->enter, this, data);
 
     this->update = this->state->update ?: _nullf;
     this->leave = this->state->leave ?: _nullf;
@@ -69,27 +68,21 @@ static const void (*const _entity_array[_DARKEN_ENTITY_STATUS_MAX])(de_entity *c
 
 //
 
-void de_manager_loop(unsigned *const loop, de_state *const loop_state, unsigned size)
+void de_manager_loop(unsigned *const loop, de_state *const state, unsigned size)
 {
-    size += sizeof(de_entity);
-    de_entity *entity = malloc(size);
-    memset(entity, 0, size);
+    de_entity *entity = malloc(size + sizeof(de_entity));
 
-    de_state *state = loop_state ?: &_empty;
-    entity->update = state->update ?: _nullf;
-    entity->leave = state->leave ?: _nullf;
-
-    _DARKEN_MANAGER_EXEC(state->enter, entity, entity->data);
+    entity->state = state;
+    _entity_set(entity, entity->data);
 
     while (*loop == 1)
     {
         _entity_array[entity->status](entity, entity->data);
     }
 
-    _DARKEN_MANAGER_EXEC(entity->leave, entity, entity->data);
+    _EXEC(entity->leave, entity, entity->data);
 
     free(entity);
-    entity = 0;
 }
 
 void de_manager_init(de_manager *const this, unsigned bytes, unsigned datasize)
@@ -131,10 +124,8 @@ void de_manager_update(de_manager *const this)
 
 void de_manager_reset(de_manager *const this)
 {
-    uclist *const list = &this->list;
-
-    uclist_iterator(list, _destroy, 1);
-    uclist_reset(list);
+    uclist_iterator(&this->list, _destroy, 1);
+    uclist_reset(&this->list);
 }
 
 void de_manager_end(de_manager *const this)
