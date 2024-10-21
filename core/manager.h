9204a001e8ee62079f082432ab323a7c394fde43
langares@yahoo.es <langares@yahoo.es>
2024-10-21 04:30:49 +0200

    no message

--- core/manager.h
+++ core/manager.h
@@ -18,6 +18,3 @@ void de_manager_end(de_manager *const);
 typedef struct de_entity de_entity;
 
 de_entity *de_manager_new(de_manager *const, void (*)());
-
-// Darken private
-void _de_manager_delete(de_manager *const, de_entity *const);
