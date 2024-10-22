1b3b635d7a7d35e745dc93b22eb733b9a0b3ebd0
langares@yahoo.es <langares@yahoo.es>
2024-10-22 23:38:26 +0200

    no message

--- priv/declarations.h
+++ priv/declarations.h
@@ -1,9 +1,8 @@
 #pragma once
 
-typedef struct de_entity de_entity;
-typedef struct de_manager de_manager;
-
 #include "../core/state.h"
+#include "../core/entity.h"
+#include "../core/manager.h"
 
 void _entity_init(de_entity *const, de_manager *const, de_state);
 void _entity_update(de_entity *const);
