c721d004a24216d5dbe4fcf52c2ce05ddbe58046
langares@yahoo.es <langares@yahoo.es>
2024-10-27 00:18:30 +0200

    no message

--- core/entity.c
+++ core/entity.c
@@ -1,15 +1,11 @@
 #include "entity.h"
 
-de_entity *de_entity_set(de_entity *const this, void (*state[static 1])())
+void de_entity_set(de_entity *const this, void (*state[static 1])())
 {
     this->state = state;
-
-    return this;
 }
 
-de_entity *de_entity_destructor(de_entity *const this, void (*destructor)())
+void de_entity_destructor(de_entity *const this, void (*destructor)())
 {
     this->destructor = destructor;
-
-    return this;
 }
--- core/entity.h
+++ core/entity.h
@@ -7,5 +7,5 @@ typedef struct
     unsigned char data[]; // Bytes for casting data & components
 } de_entity;
 
-de_entity *de_entity_set(de_entity *const, void (*[static 1])());
-de_entity *de_entity_destructor(de_entity *const, void (*)());
+void de_entity_set(de_entity *const, void (*[static 1])());
+void de_entity_destructor(de_entity *const, void (*)());
