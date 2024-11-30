e7c81c5dc82d2d71a43cbe63358c5c0e3c8ba392
langares@yahoo.es <langares@yahoo.es>
2024-11-30 03:44:37 +0100

    darken

--- darken.c
+++ darken.c
@@ -1,29 +1,18 @@
 #include "config.h"
 #include "core/entity.h"
 
-static bool loop;
-
-void darken_loop(de_state (*state)(), unsigned size)
+void darken(de_state (*state)(), unsigned size)
 {
     if (state == 0)
     {
-        loop = false;
         return;
     }
 
     de_entity *entity = malloc(sizeof(de_entity) + size);
     entity->handler = state;
 
-    loop = true;
-    while (loop == true)
-    {
-        entity->handler = entity->handler(entity->data, entity);
-    }
+    while (de_entity_update(entity))
+        ;
 
     free(entity);
-}
-
-void darken_break()
-{
-    loop = false;
 }
--- darken.h
+++ darken.h
@@ -6,8 +6,7 @@
 #include "core/manager.h"
 #include "core/system.h"
 
-void darken_loop(de_state (*)(), unsigned);
-void darken_break();
+void darken(de_state (*)(), unsigned);
 
 #define DE_CAST(DATA, TYPE) ((TYPE *const)DATA)
 #define DE_DATA(DATA, TYPE, VAR) TYPE *const VAR = DE_CAST(DATA, TYPE)
