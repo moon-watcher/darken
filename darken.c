974400ed204d05b5511c9ad70faa421bc2dc5ad3
langares@yahoo.es <langares@yahoo.es>
2024-12-13 09:06:04 +0100

    no message

--- darken.c
+++ darken.c
@@ -6,7 +6,6 @@ void darken(de_state_f state, unsigned size)
     de_entity *entity = malloc(sizeof(de_entity) + size);
 
     de_entity_state(entity, state);
-    de_entity_destructor(entity, 0);
 
     while (entity->state != 0)
     {
