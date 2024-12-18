78ac29cdb343b9f3ba9dc81a324635d95a1b7200
langares@yahoo.es <langares@yahoo.es>
2024-12-18 12:37:02 +0100

    Simple entity.c

--- core/entity.c
+++ core/entity.c
@@ -1,50 +1,39 @@
 #include "entity.h"
 #include "../config.h"
 
-#define _DARKEN_LOG(MSG)               \
-    else                               \
-    {                                  \
-        DARKEN_LOG("de_entity: " MSG); \
-    }
-
-#define _PROCESS(THIS, ASSIGN, MSG) ({ \
-    if (THIS != 0)                     \
-    {                                  \
-        ASSIGN;                        \
-    }                                  \
-    _DARKEN_LOG(MSG);                  \
-    THIS; })
+#define _PROCESS(SET, MSG)         \
+    if (this != 0)                 \
+    {                              \
+        SET;                       \
+        return this;               \
+    }                              \
+    DARKEN_LOG("de_entity: " MSG); \
+    return 0
 
 //
 
 de_state de_entity_exec(de_entity *const this)
 {
-    if (this != 0)
+    if (this == 0 || this->state == 0)
     {
-        de_state_f state = this->state;
-
-        if (state != 0)
-        {
-            return state(this->data, this);
-        }
-        _DARKEN_LOG("exec, state");
+        DARKEN_LOG("exec");
+        return 0;
     }
-    _DARKEN_LOG("exec, this");
 
-    return 0;
+    return this->state(this->data, this);
 }
 
 de_entity *de_entity_delete(de_entity *const this)
 {
-    return _PROCESS(this, this->state = 0, "delete");
+    _PROCESS(this->state = 0, "delete");
 }
 
 de_entity *de_entity_state(de_entity *const this, de_state_f state)
 {
-    return _PROCESS(this, this->state = state, "state");
+    _PROCESS(this->state = state, "state");
 }
 
 de_entity *de_entity_destructor(de_entity *const this, de_state_f destructor)
 {
-    return _PROCESS(this, this->destructor = destructor, "destructor");
+    _PROCESS(this->destructor = destructor, "destructor");
 }
