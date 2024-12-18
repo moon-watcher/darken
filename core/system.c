bc3a120b6e5b9b3918d2f95a8118e1f46d1b458e
langares@yahoo.es <langares@yahoo.es>
2024-12-18 12:35:11 +0100

    de_system_update()

--- core/system.c
+++ core/system.c
@@ -1,13 +1,13 @@
 #include "system.h"
 #include "../config.h"
 
-#define _PROCESS(RET, EXPRESSION, MSG) ({  \
-    if (EXPRESSION)                        \
-    {                                      \
-        DARKEN_LOG("de_system_add: " MSG); \
-        return RET;                        \
-    }                                      \
-    RET; })
+#define _PROCESS(EXP, OK, ERR, MSG)    \
+    if (EXP)                           \
+    {                                  \
+        DARKEN_LOG("de_system: " MSG); \
+        ERR;                           \
+    }                                  \
+    OK
 
 //
 
@@ -20,20 +20,20 @@ void *de_system_add(de_system *const this, void *const data)
 {
     void *ret = uclist_add(this, data);
 
-    return _PROCESS(ret, ret == 0, "allocation");
+    _PROCESS(ret == 0, return ret, , "allocation");
 }
 
 int de_system_delete(de_system *const this, void *const data)
 {
     int ret = uclist_remove(this, data, 0);
 
-    return _PROCESS(ret, ret == UCLIST_NOT_FOUND, "reference not found");
+    _PROCESS(ret == UCLIST_NOT_FOUND, return ret, , "reference not found");
 }
 
-int de_system_update(de_system *const this, void (*update)(), unsigned params)
+void de_system_update(de_system *const this, void (*update)(), unsigned params)
 {
-    _PROCESS(UCLIST_NO_ITERATOR, update == 0, "no iterator");
-    _PROCESS(UCLIST_NO_NBITEMS, params == 0, "params is 0");
+    _PROCESS(update == 0, , return UCLIST_NO_ITERATOR, "no iterator");
+    _PROCESS(params == 0, , return UCLIST_NO_NBITEMS, "params is 0");
 
     unsigned count = this->count;
 
@@ -41,8 +41,6 @@ int de_system_update(de_system *const this, void (*update)(), unsigned params)
     {
         update(this->list[i + 0], this->list[i + 1], this->list[i + 2], this->list[i + 3]);
     }
-
-    return count / params;
 }
 
 void de_system_reset(de_system *const this)
