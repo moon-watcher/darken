a2addee91106efad26e4debfbd058aa5037aa9bc
langares@yahoo.es <langares@yahoo.es>
2024-12-29 23:45:56 +0100

    no message

--- assert.h
+++ assert.h
@@ -5,12 +5,12 @@
 #define _DARKEN_ASSERT(EVAL, RETURN, MESSAGE) \
     _DARKEN_EQUAL(EVAL == 0, RETURN, MESSAGE)
 
-#define _DARKEN_EQUAL(CONDITION, RETURN, MESSAGE) \
-    ({                                         \
-        if (CONDITION)                         \
-        {                                      \
-            DARKEN_LOG(MESSAGE);               \
-            return RETURN;                     \
-        }                                      \
-        RETURN;                                \
+#define _DARKEN_EQUAL(EVAL, RETURN, MESSAGE) \
+    ({                                       \
+        if (EVAL)                            \
+        {                                    \
+            DARKEN_LOG(MESSAGE);             \
+            return RETURN;                   \
+        }                                    \
+        RETURN;                              \
     })
