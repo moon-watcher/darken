84357622d36cacc2f8282b3866c9da6b85cb34c3
langares@yahoo.es <langares@yahoo.es>
2024-12-29 23:42:11 +0100

    _DARKEN_EQUAL()

--- assert.h
+++ assert.h
@@ -2,12 +2,15 @@
 
 #include "config.h"
 
-#define _DARKEN_ASSERT(CONDITION, RETURN, MESSAGE) \
-    ({                                        \
-        if (CONDITION)                        \
-        {                                     \
-            DARKEN_LOG(MESSAGE);              \
-            return RETURN;                    \
-        }                                     \
-        RETURN;                               \
+#define _DARKEN_ASSERT(EVAL, RETURN, MESSAGE) \
+    _DARKEN_EQUAL(EVAL == 0, RETURN, MESSAGE)
+
+#define _DARKEN_EQUAL(CONDITION, RETURN, MESSAGE) \
+    ({                                         \
+        if (CONDITION)                         \
+        {                                      \
+            DARKEN_LOG(MESSAGE);               \
+            return RETURN;                     \
+        }                                      \
+        RETURN;                                \
     })
--- core/entity.c
+++ core/entity.c
@@ -3,14 +3,14 @@
 
 de_entity *de_entity_state(de_entity *const this, de_state_f state)
 {
-    _DARKEN_ASSERT(this == 0, 0, "Invalid entity");
+    _DARKEN_ASSERT(this, 0, "Invalid entity");
     this->state = state;
     return this;
 }
 
 de_entity *de_entity_destructor(de_entity *const this, de_state_f state)
 {
-    _DARKEN_ASSERT(this == 0, 0, "Invalid entity");
+    _DARKEN_ASSERT(this, 0, "Invalid entity");
     this->destructor = state;
     return this;
 }
