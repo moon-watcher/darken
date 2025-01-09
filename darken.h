f335c42af50e1c875cac42617e3a394f4f8b7e0a
langares@yahoo.es <langares@yahoo.es>
2025-01-09 14:40:55 +0100

    Rename macros

--- darken.h
+++ darken.h
@@ -7,5 +7,5 @@
 de_entity *darken_init(void *(*)(unsigned short), unsigned);
 void darken_loop(de_entity *const, de_state);
 
-#define DE_CAST(ENTITY, TYPE) ((TYPE *const)ENTITY->data)
-#define DE_DATA(ENTITY, TYPE, VAR) TYPE *const VAR = DE_CAST(ENTITY, TYPE)
+#define darken_cast(ENTITY, TYPE) ((TYPE *const)ENTITY->data)
+#define darken_data(ENTITY, TYPE, VAR) TYPE *const VAR = darken_cast(ENTITY, TYPE)
