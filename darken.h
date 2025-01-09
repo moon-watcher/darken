3ebbbe0ea99c5eb48a52a229f3b285549e15e26d
langares@yahoo.es <langares@yahoo.es>
2025-01-09 15:01:12 +0100

    Remove macros

--- darken.h
+++ darken.h
@@ -6,6 +6,3 @@
 
 de_entity *darken_init(void *(*)(unsigned short), unsigned);
 void darken_loop(de_entity *const, de_state);
-
-#define darken_cast(ENTITY, TYPE) ((TYPE *const)ENTITY->data)
-#define darken_data(ENTITY, TYPE, VAR) TYPE *const VAR = darken_cast(ENTITY, TYPE)
