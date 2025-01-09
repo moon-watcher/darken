7f47676fc9882cb9dc26a1018347680df1e8f52c
langares@yahoo.es <langares@yahoo.es>
2025-01-09 16:51:44 +0100

    no message

--- core/manager.c
+++ core/manager.c
@@ -16,23 +16,23 @@ void de_manager_update(de_manager *const this)
     {
         de_entity *const entity = this->list[i];
 
-        if (entity->state != 0)
+        if (entity->state == 0)
         {
-            entity->state = entity->state(entity->data, entity);
-            ++i;
+            --this->size;
+            this->list[i] = this->list[this->size];
+            this->list[this->size] = entity;
+            entity->destructor != 0 && entity->destructor(entity->data, entity);
             continue;
         }
 
-        --this->size;
-        this->list[i] = this->list[this->size];
-        this->list[this->size] = entity;
-        entity->destructor != 0 && entity->destructor(entity->data, entity);
+        entity->state = entity->state(entity->data, entity);
+        ++i;
     }
 }
 
 void de_manager_reset(de_manager *const this)
 {
-    uclist_iterator(this, (void*) de_entity_delete);
+    uclist_iterator(this, (void *)de_entity_delete);
     de_manager_update(this);
 }
 
