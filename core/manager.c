b9687b7afd14a217e55f69898b8c5bff328e0019
langares@yahoo.es <langares@yahoo.es>
2025-01-13 23:41:41 +0100

    no message

--- core/manager.c
+++ core/manager.c
@@ -1,8 +1,9 @@
 #include "manager.h"
+#include "../darken.h"
 
 void de_manager_init(de_manager *const this, unsigned bytes)
 {
-    uclist_init(this, sizeof(de_entity) + bytes);
+    uclist_init(this, DARKEN_SIZE(bytes));
 }
 
 de_entity *de_manager_new(de_manager *const this, de_state state)
