c3dd8e529bcf5e72510d1a95bd423fd8e08a9f5d
AFRODITA\Daniel <langares@yahoo.es>
2025-11-05 12:56:05 +0100

    no message

--- darken.c
+++ darken.c
@@ -1,24 +1,13 @@
 #include "darken.h"
 
-
-// State
-
-void *de_state_empty()
+void darken_init(de_manager *const this, unsigned bytes)
 {
-    return de_state_empty;
+    uclist_init_alloc(this, sizeof(de_entity) + bytes);
 }
 
-
-// Manager
-
-void de_manager_init(de_manager *const this, unsigned bytes)
+de_entity *darken_new(de_manager *const this, de_state state, de_state destructor)
 {
-    uclist_init_alloc(&this->list, sizeof(de_entity) + bytes);
-}
-
-de_entity *de_manager_new(de_manager *const this, de_state state, de_state destructor)
-{
-    de_entity *const entity = uclist_alloc(&this->list);
+    de_entity *const entity = uclist_alloc(this);
 
     entity->state = state;
     entity->destructor = destructor;
@@ -26,55 +15,32 @@ de_entity *de_manager_new(de_manager *const this, de_state state, de_state destr
     return entity;
 }
 
-void de_manager_update(de_manager *const this)
+void darken_update(de_manager *const this)
 {
-    de_entity **const items = de_manager_getItems(this);
-    unsigned i = 0, size = de_manager_getSize(this);
+    de_entity **const items = (de_entity **)this->items;
 
-    while (i < size)
+    for (unsigned i = 0; i < this->size;)
     {
         de_entity *const entity = items[i++];
-        de_state state = entity->state;
 
-        if (state == de_state_empty)
-            continue;
-
-        if (!state)
+        if (entity->state)
+            entity->state = entity->state(entity->data);
+        else
         {
-            --size;
-            uclist_removeByIndex(&this->list, --i);
-            state = entity->destructor ?: de_state_empty;
+            uclist_removeByIndex(this, --i);
+            entity->destructor && entity->destructor(entity->data);
         }
-
-        entity->state = state(entity->data);
     }
 }
 
-void de_manager_reset(de_manager *const this)
+void darken_reset(de_manager *const this)
 {
-    uclist_iterator(&this->list, ({ void d(de_entity *e) { e->state = 0; }; d; }));
-    de_manager_update(this);
+    uclist_iterator(this, ({ void d(de_entity *e) { e->state = 0; }; d; }));
+    darken_update(this);
 }
 
-void de_manager_end(de_manager *const this)
+void darken_end(de_manager *const this)
 {
-    de_manager_reset(this);
-    uclist_end(&this->list);
-}
-
-//
-
-inline unsigned de_manager_getSize(de_manager *const this)
-{
-    return this->list.size;
-}
-
-inline unsigned de_manager_getCapacity(de_manager *const this)
-{
-    return this->list.capacity;
-}
-
-inline de_entity **de_manager_getItems(de_manager *const this)
-{
-    return (de_entity **)this->list.items;
+    darken_reset(this);
+    uclist_end(this);
 }
--- darken.h
+++ darken.h
@@ -2,39 +2,19 @@
 
 #include "config.h"
 
-
-// State
-
 typedef void *(*de_state)(void *const);
 
-void *de_state_empty();
-
-
-// Entity
-
-typedef struct de_entity
+typedef struct
 {
     de_state state;
     de_state destructor;
     unsigned char data[];
 } de_entity;
 
+typedef uclist de_manager;
 
-// Manager
-
-typedef struct de_manager
-{
-    uclist list;
-} de_manager;
-
-void de_manager_init(de_manager *const, unsigned);
-de_entity *de_manager_new(de_manager *const, de_state, de_state);
-void de_manager_update(de_manager *const);
-void de_manager_reset(de_manager *const);
-void de_manager_end(de_manager *const);
-
-//
-
-unsigned de_manager_getSize(de_manager *const);
-unsigned de_manager_getCapacity(de_manager *const);
-de_entity **de_manager_getItems(de_manager *const);
+void darken_init(de_manager *const, unsigned);
+de_entity *darken_new(de_manager *const, de_state, de_state);
+void darken_update(de_manager *const);
+void darken_reset(de_manager *const);
+void darken_end(de_manager *const);
